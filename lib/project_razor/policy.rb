39d57401765dab3f5357b13c2b6316a66b4cf6cd
Antony Messerli <amesserl@rackspace.com>
2013-03-05 23:54:41 -0600

    Citrix XenServer Support for 6.1 (tampa) and 6.0 (boston)
    Adds the ability to provision Citrix XenServer.  Model configuration handles
    generation of the xenserverinstall_xml answerfile which it uses during installation.
    
    pxelinux.0 is chainloaded to boot the install instead of iPXE due to mboot.32
    complications which will be looked at in the future.  It calls back for a generated
    isolinux file to obtain it's boot settings for the installation.

--- lib/project_razor/policy.rb
+++ lib/project_razor/policy.rb
@@ -2,6 +2,7 @@ require "project_razor/policy/base"
 require "project_razor/policy/boot_mk"
 require "project_razor/policy/linux_deploy"
 require "project_razor/policy/vmware_hypervisor"
+require "project_razor/policy/xenserver_hypervisor"
 
 module ProjectRazor
   module Policy
