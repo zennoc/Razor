e5cd917f50bc28657ea0f8ae332b928f3f313736
Daniel Pittman <daniel@rimspace.net>
2013-04-03 14:34:31 -0700

    Merge `SliceUtil::Common` module into the one place it is used
    The code from `SliceUtil::Common` was actually part of the base `Slice` class,
    but was extracted into a separate module.
    
    Despite that, it was extremely intimate with the behaviours of the slice
    class, to the point of depending on the exact member variable structure, and
    the call sequence of various components.
    
    This merges that code directly into the base class, making it much clearer
    that this is not in any meaningful way distinct code from the one consumer.
    
    There should be no functional changes from this, simply a few lines of
    boilerplate code removed and a less confusing overall layout of the code.
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- spec/project_razor/slice_spec.rb
+++ spec/project_razor/slice_spec.rb
@@ -2,11 +2,60 @@ require 'spec_helper'
 require 'project_razor/slice'
 
 describe ProjectRazor::Slice do
-  # REVISIT: this is a stub - we test that we can load the code, which is
-  # better than no testing at all, but not actually very interesting in the
-  # bigger picture.  When we write any real test for the class then this
-  # next to useless test should be removed. --daniel 2013-02-21
-  it "should exist as a constant" do
-    described_class.should be
+  # before :each do
+  #   @test = TestClass.new
+  #   @test.extend(ProjectRazor::SliceUtil::Common)
+  #   # TODO: Review external dependencies here:
+  #   @test.extend(ProjectRazor::Utility)
+  # end
+
+  context "code formerly known as SliceUtil::Common" do
+    describe "get_web_args" do
+      it "should return value for matching key" do
+        ProjectRazor::Slice.new(['{"@k1":"v1","@k2":"v2","@k3":"v3"}']).
+          get_web_vars(['k1', 'k2']).should == ['v1','v2']
+      end
+
+      it "should return nil element for nonmatching key" do
+        ProjectRazor::Slice.new(['{"@k1":"v1","@k2":"v2","@k3":"v3"}']).
+          get_web_vars(['k1', 'k4']).should == ['v1', nil]
+      end
+
+      it "should return nil for invalid JSON" do
+        ProjectRazor::Slice.new(['\3"}']).get_web_vars(['k1', 'k2']).should == nil
+      end
+    end
+
+    describe "get_cli_args" do
+      TestArgs = [
+        "template=debian_wheezy",
+        "label=debian",
+        "image_uuid=3RpS0x2KWmITuAsHALa3Ni"
+      ]
+
+      it "should return value for matching key" do
+        ProjectRazor::Slice.new(TestArgs).get_cli_vars(['template', 'label']).
+          should == ['debian_wheezy', 'debian']
+      end
+
+      it "should return nil element for nonmatching key" do
+        ProjectRazor::Slice.new(TestArgs).get_cli_vars(['template', 'foo']).
+          should == ['debian_wheezy', nil]
+      end
+    end
+
+    describe "validate_arg" do
+      subject('slice') { ProjectRazor::Slice.new([]) }
+      it "should return false for empty values" do
+        [ nil, {}, '', '{}', '{1}', ['', 1], [nil, 1], ['{}', 1] ].each do |val|
+          slice.validate_arg(*[val].flatten).should == false
+        end
+      end
+
+      it "should return valid value" do
+        slice.validate_arg('foo','bar').should == ['foo', 'bar']
+      end
+    end
+
   end
 end
